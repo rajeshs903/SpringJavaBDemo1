b610c13d045ffaea65917ccf99ca19511b64f5b9
Rajesh Santha <rajesh.santha29@gmail.com>
2017-12-27 00:04:15 +0530

    constructor injection commit

--- src/org/rajesh/springjb/DrawinhApp.java
+++ src/org/rajesh/springjb/DrawinhApp.java
@@ -23,3 +23,22 @@ public class DrawinhApp {
 	}
 
 }
+
+
+/*
+ * 
+ *
+ http://blog.pawandubey.com/dependency-injection-a-twenty-five-dollar-term/ 
+ 
+ Dependency Injection is nothing but passing the instance variables of a class in its constructor. 
+ So, if we have a class,
+ 
+ public class NeedsInjection{
+  Dependency injection;
+    NeedsInjection(){
+     injection = new Dependency();
+    }
+} 
+ 
+ */
+*/
--- src/org/rajesh/springjb/Triangle.java
+++ src/org/rajesh/springjb/Triangle.java
@@ -2,16 +2,41 @@ package org.rajesh.springjb;
 
 public class Triangle {
 	private String type;
+private int height;
+	
+	public Triangle(String type)
+	{
+		this.type=type;
+	}
+
+	public Triangle(int height)
+	{
+		this.height=height;
+	}
+	
+	public Triangle(String type,int height) {
+	this.type=type;
+	this.height=height;
+	}
+	
+	
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
 
 	public String getType() {
 		return type;
 	}
 
-	public void setType(String type) {
+	/*public void setType(String type) {
 		this.type = type;
 	}
-
+*/
 	public void draw() {
-		System.out.println("Triangle drawn as "+type);
+		System.out.println("Triangle drawn as "+type+" \n and height is "+height+" meters");
 	}
 }
