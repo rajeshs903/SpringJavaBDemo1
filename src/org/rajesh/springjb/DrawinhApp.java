6f11a31b70a234c548ad88ad6cdc079b4ed7d830
Rajesh Santha <rajesh.santha29@gmail.com>
2017-12-26 20:08:54 +0530

    intial commit for Spring lessons JavaB

---
+++ src/org/rajesh/springjb/DrawinhApp.java
@@ -0,0 +1,22 @@
+package org.rajesh.springjb;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
+public class DrawinhApp {
+
+	public static void main(String[] args) {
+		//Triangle triangle=new Triangle(); // normal way of creating objects
+		
+		
+		BeanFactory factory=new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		Triangle triangle =(Triangle) factory.getBean("triangle");
+		
+		
+		triangle.draw();
+
+		
+	}
+
+}
---
+++ src/org/rajesh/springjb/Triangle.java
@@ -0,0 +1,9 @@
+package org.rajesh.springjb;
+
+public class Triangle {
+
+	public void draw()
+	{
+		System.out.println("Triangle drawn");
+	}
+}
