c34e9daa380d28aef16bbe0a217cdc15f728dca4
Rajesh Santha <rajesh.santha29@gmail.com>
2017-12-27 23:28:10 +0530

    Dependecy injection for refernce variables- PointA,PointB,PointC intialization with x,y - Video 07

--- src/org/rajesh/springjb/Triangle.java
+++ src/org/rajesh/springjb/Triangle.java
@@ -1,42 +1,46 @@
 package org.rajesh.springjb;
 
 public class Triangle {
-	private String type;
-private int height;
-	
-	public Triangle(String type)
-	{
-		this.type=type;
-	}
 
-	public Triangle(int height)
-	{
-		this.height=height;
-	}
-	
-	public Triangle(String type,int height) {
-	this.type=type;
-	this.height=height;
-	}
+	private  Point pointA;
+	private Point pointB;
+	private Point pointC;
 	
 	
-	public int getHeight() {
-		return height;
+	public Point getPointA() {
+		return pointA;
 	}
 
-	public void setHeight(int height) {
-		this.height = height;
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
 	}
 
-	public String getType() {
-		return type;
+
+	public Point getPointB() {
+		return pointB;
 	}
 
-	/*public void setType(String type) {
-		this.type = type;
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
 	}
-*/
+
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
+
 	public void draw() {
-		System.out.println("Triangle drawn as "+type+" \n and height is "+height+" meters");
+		System.out.println("Triangle coordinates are");
+		System.out.println("Point A is x= "+pointA.getX()+" and y="+pointA.getY());
+		System.out.println("Point A is x= "+pointB.getX()+" and y="+pointB.getY());
+		System.out.println("Point A is x= "+pointC.getX()+" and y="+pointC.getY());
 	}
 }
