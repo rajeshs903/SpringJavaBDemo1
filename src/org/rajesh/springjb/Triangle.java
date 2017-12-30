1ed784f926d50be25c7c2386e0a33de0ef5d98c4
Rajesh Santha <rajesh.santha29@gmail.com>
2017-12-30 23:35:10 +0530

    video 9 - JavaBrains explained idref concept incorrectly. ignoring the changes commit.

--- src/org/rajesh/springjb/Triangle.java
+++ src/org/rajesh/springjb/Triangle.java
@@ -1,32 +1,46 @@
 package org.rajesh.springjb;
 
-import java.util.List;
-
 public class Triangle {
 
-	private  List<Point> points;	//instead of pointA pointB pointC.. Taken List.. example for Collection dependency injection
+	private  Point pointA;
+	private Point pointB;
+	private Point pointC;
 	
 	
-	
-	
-	public List<Point> getPoints() {
-		return points;
+	public Point getPointA() {
+		return pointA;
 	}
 
 
-
-
-	public void setPoints(List<Point> points) {
-		this.points = points;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
 	}
 
 
+	public Point getPointB() {
+		return pointB;
+	}
+
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
 
 
 	public void draw() {
 		System.out.println("Triangle coordinates are");
-		for (Point point:points)
-			System.out.println("Point A is x= "+point.getX()+" and y="+point.getY());
-		
+		System.out.println("Point A is x= "+pointA.getX()+" and y="+pointA.getY());
+		System.out.println("Point A is x= "+pointB.getX()+" and y="+pointB.getY());
+		System.out.println("Point A is x= "+pointC.getX()+" and y="+pointC.getY());
 	}
 }
