254155dc2b42902ee253b8f5bf7a60674e8333b6
Rajesh Santha <rajesh.santha29@gmail.com>
2018-01-01 17:25:44 +0530

    autowiring byName video - 10

--- src/org/rajesh/springjb/Triangle.java
+++ src/org/rajesh/springjb/Triangle.java
@@ -2,31 +2,44 @@ package org.rajesh.springjb;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Triangle {
+	
+	
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 
-	private  List<Point> points;	//instead of pointA pointB pointC.. Taken List.. example for Collection dependency injection
-	
-	
-	
-	
-	public List<Point> getPoints() {
-		return points;
+	public Point getPointA() {
+		return pointA;
 	}
 
-
-
-
-	public void setPoints(List<Point> points) {
-		this.points = points;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
 	}
 
+	public Point getPointB() {
+		return pointB;
+	}
 
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
 
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
 
 	public void draw() {
-		System.out.println("Triangle coordinates are");
-		for (Point point:points)
-			System.out.println("Point A is x= "+point.getX()+" and y="+point.getY());
+				System.out.println("Triangle coordinates are");
+				System.out.println("Point A is x= "+pointA.getX()+" and y="+pointA.getY());
+				System.out.println("Point A is x= "+pointB.getX()+" and y="+pointB.getY());
+				System.out.println("Point A is x= "+pointC.getX()+" and y="+pointC.getY());
 		
 	}
 }
