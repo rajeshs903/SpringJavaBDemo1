2ae20c6724f9ed6b26a936969af367a3820cdb30
Rajesh Santha <rajesh.santha29@gmail.com>
2018-01-01 22:48:08 +0530

    video 13 (part 2) - Inheritance with bean definetion .. Merging the list item instead of overrding it with child item .. using abstract parent to make the parent bean definetion as a template to its child rather than standalone bean definetion

--- src/org/rajesh/springjb/Triangle.java
+++ src/org/rajesh/springjb/Triangle.java
@@ -1,61 +1,43 @@
 package org.rajesh.springjb;
 
-import org.springframework.beans.BeansException;
+import java.util.List;
+
 import org.springframework.beans.factory.BeanNameAware;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 
-public class Triangle implements ApplicationContextAware,BeanNameAware{
-	//When spring instantiates beans, it looks for a couple of interfaces like ApplicationContextAware and InitializingBean. If they are found, the methods are invoked
+public class Triangle implements BeanNameAware {
+
+	private  List<Point> points;	//instead of pointA pointB pointC.. Taken List.. example for Collection dependency injection
 	
-	private Point pointA;
-	private Point pointB;
-	private Point pointC;
-	private ApplicationContext context;
-
-	public Point getPointA() {
-		return pointA;
+	
+	
+	
+	public List<Point> getPoints() {
+		return points;
 	}
 
-	public void setPointA(Point pointA) {
-		this.pointA = pointA;
+
+
+
+	public void setPoints(List<Point> points) {
+		this.points = points;
 	}
 
-	public Point getPointB() {
-		return pointB;
-	}
 
-	public void setPointB(Point pointB) {
-		this.pointB = pointB;
-	}
 
-	public Point getPointC() {
-		return pointC;
-	}
-
-	public void setPointC(Point pointC) {
-		this.pointC = pointC;
-	}
 
 	public void draw() {
-				System.out.println("Triangle coordinates are");
-				System.out.println("Point A is x= "+pointA.getX()+" and y="+pointA.getY());
-				System.out.println("Point A is x= "+pointB.getX()+" and y="+pointB.getY());
-				System.out.println("Point A is x= "+pointC.getX()+" and y="+pointC.getY());
+		System.out.println("Triangle coordinates are");
+		for (Point point:points)
+			System.out.println("Point is x= "+point.getX()+" and y="+point.getY());
 		
 	}
+
+
+
 
 	@Override
-	public void setApplicationContext(ApplicationContext context) throws BeansException {
-		this.context=context;
+	public void setBeanName(String arg0) {
+		System.out.println(arg0.toString());
 		
 	}
-
-	@Override
-	public void setBeanName(String beanName) {
-		System.out.println("bean name is "+beanName);
-		
-	}
-
-	
 }
